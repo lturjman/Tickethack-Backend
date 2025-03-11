02e01dd23b6afd6bc13c948d6a5c558bc6587b26
Laura Turjman <contact@lauraturjman.fr>
2025-03-11 12:02:13 +0100

    config models et routes

--- routes/trips.js
+++ routes/trips.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
-require("./models/connection");
-const Trip = require("./models/trips");
+require("../models/connection");
+const Trip = require("../models/trips");
 
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
