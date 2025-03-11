2250fe11efc7bfa1502dd19ed2227821acb3f7ce
Laura Turjman <contact@lauraturjman.fr>
2025-03-11 11:18:18 +0100

    BDD & connectionString

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+require("dotenv").config();
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+const tripsRouter = require("./routes/trips");
 
 var app = express();
 const cors = require("cors");
@@ -18,5 +20,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/trips", tripsRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const moment = require("moment");
+const Trip = require("./trips");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
