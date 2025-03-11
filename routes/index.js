bb599caf13d0ffea36480186732deb8ed1df8b56
Laura Turjman <contact@lauraturjman.fr>
2025-03-11 10:09:35 +0100

    confit moment

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/api/message", function (req, res) {
+  res.json({ message: "Hello depuis le backend!" });
+});
+
+const moment = require("moment");
+
+router.get("/api/date", (req, res) => {
+  const currentDate = moment().format("YYYY-MM-DD HH:mm:ss");
+  res.json({ date: currentDate });
 });
 
 module.exports = router;
