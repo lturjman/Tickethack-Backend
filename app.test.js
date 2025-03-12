1aa1d18c1963a8e3f4d452d037966783a27a9cf9
Laura Turjman <contact@lauraturjman.fr>
2025-03-12 15:21:00 +0100

    fix bookings

---
+++ app.test.js
@@ -0,0 +1,11 @@
+// const request = require("supertest");
+// const app = require("./app");
+// const mongoose = require("mongoose");
+// const Cart = require("../models/Cart"); // Assurez-vous que le modèle Cart est correctement importé
+
+// it("GET /search", async () => {
+//   const res = await request(app).get("/Cart");
+
+//   expect(res.statusCode).toBe(200);
+//   expect(res.body.stock).toEqual(["..."]);
+// });
